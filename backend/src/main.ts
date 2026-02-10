72cf1d3c2ba123cc4672206755ec84eac7d7f1aa
Hasala Gayendra <hashalagayendra@gmail.com>
2026-02-10 20:32:56 +0530

    Add Swagger support for API documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder } from '@nestjs/swagger/dist/document-builder';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Task Productivity app')
+    .setDescription('The productivity API description')
+    .setVersion('1.0')
+    .addTag('task productivity app')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
