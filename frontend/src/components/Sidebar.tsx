8afc5e6feb5f60effdfc50a53f3b421949d2d044
Hasala Gayendra <hashalagayendra@gmail.com>
2026-02-10 11:35:24 +0530

    Add react router

---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Sidebar() {
+  return <div className="w-1/5 h-screen bg-blue-600">Sidebar</div>;
+}
+
+export default Sidebar;
---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+
+function Dashboard() {
+  return (
+    <div className="bg-red-800">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default Dashboard;
